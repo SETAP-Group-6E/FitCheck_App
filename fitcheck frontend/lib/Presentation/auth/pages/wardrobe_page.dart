8f56c118cf55148c7d60444a7cd95b00a20a30a8
Sean Oijukye <s.oijukye@gmail.com>
2026-02-10 23:14:11 +0000

    create BacklightGradient class with customizable light properties and integrate it into WardrobePage

--- fitcheck frontend/lib/Presentation/auth/pages/wardrobe_page.dart
+++ fitcheck frontend/lib/Presentation/auth/pages/wardrobe_page.dart
@@ -1,19 +1,39 @@
+import 'dart:ui';
+import 'package:fitcheck/Presentation/app_style/backlight_gradient.dart';
 import 'package:flutter/material.dart';
 
 class WardrobePage extends StatelessWidget {
-  const WardrobePage({Key? key}) : super(key: key);
+  const WardrobePage({super.key});
 
   @override
   Widget build(BuildContext context) {
 	return Scaffold(
-	  appBar: AppBar(
-		title: const Text('Wardrobe'),
-	  ),
-	  body: const Center(
-		
-		child: Text('Welcome to the Wardrobe Page!'),
-	  ),
-	);
+		body: BacklightGradient(
+			colorBg: const Color.fromRGBO(59, 44, 32, 1),
+
+    light1: const [
+      Color.fromRGBO(255, 255, 255, 1),
+      Color.fromRGBO(59, 44, 32, 1),
+      Color.fromRGBO(91, 91, 91, 0.3),
+    ],
+
+    light1Alignment: const Alignment(0.8, -0.8),
+    light1Radius: 2,
+
+    light2: const [
+      Color.fromRGBO(192, 192, 192, 0.2),
+      Color.fromRGBO(59, 44, 32, 0.3),
+      Color.fromRGBO(59, 44, 32, 1),
+    ],
+
+    light2Alignment: const Alignment(-1, 1),
+    light2Radius: 1.5,
+
+    blur: 90,
+
+    child: Text('Wardrobe Page'),
+  ),
+);
   }
 }
 
