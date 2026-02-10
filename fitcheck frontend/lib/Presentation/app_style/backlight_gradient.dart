1e8cc3e66d78695433fe6c8fec5dad056659c871
Sean Oijukye <s.oijukye@gmail.com>
2026-02-10 23:14:11 +0000

    create BacklightGradient class with customizable light properties and integrate it into WardrobePage

--- fitcheck frontend/lib/Presentation/app_style/backlight_gradient.dart
+++ fitcheck frontend/lib/Presentation/app_style/backlight_gradient.dart
@@ -20,4 +20,92 @@ class BacklightGradient extends StatelessWidget {
   
   final double blur;
 
- 
+  const BacklightGradient({
+    super.key,
+    required this.child,
+
+    // Background
+    this.colorBg = const Color.fromARGB(255, 40, 29, 18),
+
+    // light 1 (center → edge)
+    this.light1 = const [
+      Color.fromARGB(255, 38, 38, 38), 
+      Color.fromARGB(255, 76, 71, 65), 
+      Color.fromARGB(0, 255, 152, 0), 
+    ],
+    this.light1Alignment = const Alignment(-0.6, -0.5),
+    this.light1Radius = 1.2,
+
+    // light 2
+    this.light2 = const [
+      Color.fromARGB(255, 38, 38, 38),
+      Color.fromARGB(255, 76, 71, 65),
+      Color.fromARGB(0, 255, 152, 0),
+    ],
+    this.light2Alignment = const Alignment(0.6, 0.4),
+    this.light2Radius = 0.9,
+
+    // Blur
+    this.blur = 80,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+
+        // Base
+        Container(color: colorBg),
+
+        // light 1
+        _buildBacklight(
+          colors: light1,
+          alignment: light1Alignment,
+          radius: light1Radius,
+        ),
+
+        // light 2
+        _buildBacklight(
+          colors: light2,
+          alignment: light2Alignment,
+          radius: light2Radius,
+        ),
+
+        // Blur
+        BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: blur,
+            sigmaY: blur,
+          ),
+          child: Container(
+            color: Colors.black.withValues(alpha: 0.04),
+          ),
+        ),
+
+        // UI
+        child,
+      ],
+    );
+  }
+
+  Widget _buildBacklight({
+    required List<Color> colors,
+    required Alignment alignment,
+    required double radius,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          center: alignment,
+          radius: radius,
+
+          // Important: multi-stop gradient
+          stops: const [0.0, 0.4, 1.0],
+
+          colors: colors,
+        ),
+      ),
+    );
+  }
+}
