b0c364ac109d966240c8f03cba14e60410ad89c3
Maheer Basit <maheerbasitahmed@gmail.com>
2026-02-14 15:57:14 +0000

    Implement login functionality and update registration page title

--- fitcheck frontend/lib/Domain/repositories/auth_repository.dart
+++ fitcheck frontend/lib/Domain/repositories/auth_repository.dart
@@ -4,5 +4,8 @@ abstract class AuthRepository {
     required String password,
     required String name,
   });
-  //Future<void> signIn({required String email, required String password});
+  Future<void> signIn({
+    required String email, 
+    required String password
+  });
 }
