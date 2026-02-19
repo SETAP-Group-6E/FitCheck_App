1f962fd8dfe148a799f0b3b3ba408160302c07eb
Sean Oijukye <s.oijukye@gmail.com>
2026-02-19 12:06:57 +0000

    Add registration page UI components;  implement password field, add google_fonts package and update main app routing

--- fitcheck frontend/lib/Presentation/App/app_pages/reg_page.dart
+++ fitcheck frontend/lib/Presentation/App/app_pages/reg_page.dart
@@ -1,7 +1,5 @@
-import 'package:fitcheck/Presentation/App/app_style/dashed_box.dart';
-import 'package:fitcheck/Presentation/App/app_style/search_bar.dart';
-import 'package:fitcheck/Presentation/app/app_style/glass_frame.dart';
-import 'package:fitcheck/Presentation/App/app_style/floating_navBar.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:fitcheck/Presentation/App/app_style/password_field.dart';
 import 'package:flutter/material.dart';
 
 class RegPage extends StatelessWidget {
@@ -16,7 +14,6 @@ class RegPage extends StatelessWidget {
           Expanded(child: SizedBox()),
           Column(
             children: [
-              
               Padding(
                 padding: const EdgeInsets.only(top: 50),
                 child: Container(
@@ -31,9 +28,153 @@ class RegPage extends StatelessWidget {
               ),
               SizedBox(height: 20),
 
-              
+              Text(
+                "Join the Community",
+                style: GoogleFonts.dmSerifText(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  
+                ),
+              ),
+              Text(
+                "Discover and share your daily style",
+                style: GoogleFonts.hedvigLettersSerif(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  
+                ),
+              ),
+              SizedBox(height: 30),
+              SizedBox(
+                height: 40,
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                    hintStyle: TextStyle(color: Colors.white54),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5)),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                    hintStyle: TextStyle(color: Colors.white54),
+                  ),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 40,
+                width: 350,
+                child: PasswordField(),
+              ),
+              SizedBox(height: 10),
+
+              SizedBox(
+                height: 40,
+                width: 350,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(217, 156, 19, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  onPressed: () {
+                    null;
+                  },
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: "Hedvig Serif Text",
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(217, 156, 19, 1),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    height: 35,
+                    width: 35,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(
+                        Icons.one_x_mobiledata,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        null;
+                      },
+                    ),
+                  ),
+                  SizedBox(width: 25),
+                  Container(
+                    decoration: BoxDecoration(
+                      color:Color.fromRGBO(217, 156, 19, 1),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    height: 35,
+                    width: 35,
+                    child: IconButton(
+                      padding: EdgeInsets.zero,
+                      icon: Icon(
+                        Icons.apple,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      onPressed: () {
+                        null;
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.white54),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Color.fromRGBO(217, 156, 19, 1)),
+                    ),
+                  ),
+                ],
+              ),
             ],
-          ),Expanded(child: SizedBox()),
+          ),
+          Expanded(child: SizedBox()),
         ],
       ),
     );
