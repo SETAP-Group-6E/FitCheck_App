92763edf0df520bef407861e88c50b5c9cf1db76
Rumesere Maku-Kemi <r.mkmakukemi@gmail.com>
2026-02-22 23:48:15 +0000

    make the ui for create item

--- fitcheck frontend/lib/main.dart
+++ fitcheck frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'Presentation/auth/pages/register_page.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'Presentation/auth/pages/login_page.dart';
 
