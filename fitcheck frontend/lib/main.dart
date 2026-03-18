88e3c1da077b047816731ad08072c4484a6c34d9
Sean Oijukye <s.oijukye@gmail.com>
2026-03-18 11:26:15 +0000

    Fix home page route by setting HomePage as the initial widget

--- fitcheck frontend/lib/main.dart
+++ fitcheck frontend/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       
-       home: const HPage(),
+       home: const HomePage(),
       
 
       routes: {
